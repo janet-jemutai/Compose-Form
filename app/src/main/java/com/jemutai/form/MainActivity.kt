807292f795785d5fcbae04da48b4314938bff3fc
Jemutai <janetjemutai19@gmail.com>
2024-04-22 16:01:01 +0300

    Submission button

--- app/src/main/java/com/jemutai/form/MainActivity.kt
+++ app/src/main/java/com/jemutai/form/MainActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -18,6 +25,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,74 +46,106 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun  AddRefugee(){
+fun  AddRefugee() {
 
     var firstname by remember { mutableStateOf("") }
-    var  middlename by remember { mutableStateOf("") }
-    var surname by  remember { mutableStateOf("") }
-    var  nationalid by remember { mutableStateOf("") }
-    var gender by  remember { mutableStateOf("") }
-    var  phonenumber by remember { mutableStateOf("") }
+    var middlename by remember { mutableStateOf("") }
+    var surname by remember { mutableStateOf("") }
+    var nationalid by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
+    var phonenumber by remember { mutableStateOf("") }
     var nationality by remember { mutableStateOf("") }
 
 
     Surface(
         modifier = Modifier
             .background(color = Color.Red)
-            .fillMaxWidth()
+            .fillMaxHeight()
     ) {
-Column() {
-    Text(text = "Add Refugee")
-}
-        Column() {
-            
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Beneficiary Personal Details",
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color.White,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
             OutlinedTextField(
                 value = firstname,
                 onValueChange = { firstname = it },
                 label = { Text("First Name") },
-                modifier = Modifier.padding(bottom = 16.dp)
+                modifier = Modifier.padding(bottom = 16.dp),
+                shape = RoundedCornerShape(20.dp)
             )
             OutlinedTextField(
                 value = middlename,
                 onValueChange = { middlename = it },
                 label = { Text("Middle Name") },
-                modifier = Modifier.padding(bottom = 16.dp)
+                modifier = Modifier.padding(bottom = 16.dp),
+                shape = RoundedCornerShape(20.dp)
+
             )
             OutlinedTextField(
                 value = surname,
                 onValueChange = { surname = it },
                 label = { Text("Sir Name") },
-                modifier = Modifier.padding(bottom = 16.dp)
+                modifier = Modifier.padding(bottom = 16.dp),
+                shape = RoundedCornerShape(20.dp)
+
             )
             OutlinedTextField(
                 value = nationalid,
                 onValueChange = { nationalid = it },
                 label = { Text("National Identification Number") },
-                modifier = Modifier.padding(bottom = 16.dp)
+                modifier = Modifier.padding(bottom = 16.dp),
+                shape = RoundedCornerShape(20.dp)
+
             )
             OutlinedTextField(
                 value = gender,
                 onValueChange = { gender = it },
                 label = { Text("Gender") },
-                modifier = Modifier.padding(bottom = 16.dp)
+                modifier = Modifier.padding(bottom = 16.dp),
+                shape = RoundedCornerShape(20.dp)
+
             )
             OutlinedTextField(
                 value = phonenumber,
                 onValueChange = { phonenumber = it },
                 label = { Text("Phone Number") },
-                modifier = Modifier.padding(bottom = 16.dp)
+                modifier = Modifier.padding(bottom = 16.dp),
+                shape = RoundedCornerShape(20.dp)
+
             )
             OutlinedTextField(
                 value = nationality,
                 onValueChange = { nationality = it },
                 label = { Text("Nationality") },
-                modifier = Modifier.padding(bottom = 16.dp)
+                modifier = Modifier.padding(bottom = 16.dp),
+                shape = RoundedCornerShape(20.dp)
+
             )
-            
+
+
+            Button(onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(16.dp),
+              // colors = ButtonColors = buttonColors(Color.Red)
+
+            ) {
+Text(text = "Submit")
+            }
+
         }
 
     }
 }
+
 
 
 //@Preview(showBackground = true)
